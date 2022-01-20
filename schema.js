49f7c6d0aeaaa7f01dce0877c0adfc58b1a86485
Francois ROLAND <froland@froland.be>
2022-01-20 17:30:22 +0000

    Create DB schema.

--- schema.js
+++ schema.js
@@ -1,5 +1,5 @@
-var debug = require('debug')('exam-express-server:schema');
-const { Sequelize } = require("sequelize");
+var debug = require("debug")("exam-express-server:schema");
+const { Sequelize, DataTypes } = require("sequelize");
 
 const sequelize = new Sequelize({
   dialect: process.env.DB_DIALECT,
@@ -8,15 +8,27 @@ const sequelize = new Sequelize({
   database: process.env.DB_DB,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  logging: (msg) => debug(msg)
+  logging: (msg) => debug(msg),
+});
+
+const Instance = sequelize.define("Instance", {
+  instanceId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  pingReceived: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    defaultValue: 0,
+  },
 });
 
 (async () => {
   try {
     await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
+    console.log("Connection has been established successfully.");
+    await sequelize.sync({ force: true });
+    //await sequelize.sync();
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.error("Error with the database:", error);
   }
 })();
-
