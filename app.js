79d037fee752a2ddadfe85c70a18abc21ff75f20
Francois ROLAND <froland@froland.be>
2022-01-20 18:40:44 +0000

    Get instance from AWS metadata API.

--- app.js
+++ app.js
@@ -1,14 +1,46 @@
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-require('./schema');
+var debug = require('debug')('exam-express-server:app');
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const axios = require("axios");
+require("./schema");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+const getInstanceId = async () => {
+  if (process.env.NODE_ENV === "development") {
+    return "LOCAL DEV";
+  } else {
+    try {
+      const tokenResponse = await axios.put(
+        "http://169.254.169.254/latest/api/token",
+        {
+          headers: { "X-aws-ec2-metadata-token-ttl-seconds": "21600" },
+        }
+      );
+      const token = tokenResponse.data;
+      debug(`AWS API token: ${token}`);
+      const metadataResponse = await axios.get(
+        "http://169.254.169.254/latest/meta-data/instance-id",
+        {
+          headers: { "X-aws-ec2-metadata-token": token },
+        }
+      );
+      return metadataResponse.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
 
+(async () => {
+  const instanceId = await getInstanceId();
+  debug(`Instance id: ${instanceId}`);
+
+})();
 
 module.exports = app;
