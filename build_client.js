7a1b7c3780fe5920d3ac8aacf4271880d34546e1
Francois ROLAND <froland@froland.be>
2022-01-28 09:03:47 +0000

    Don't run build steps asynchronously.

--- build_client.js
+++
@@ -1,8 +0,0 @@
-const child_process = require("child_process");
-
-let args = ["ci", "--production"];
-const opts = { stdio: "inherit", cwd: "client", shell: true };
-child_process.spawn("npm", args, opts);
-
-args = ["run", "build"];
-child_process.spawn("npm", args, opts);
--- build_server.js
+++
@@ -1,3 +0,0 @@
-const args = ["ci", "--production"];
-const opts = { stdio: "inherit", cwd: ".", shell: true };
-require("child_process").spawn("npm", args, opts);
